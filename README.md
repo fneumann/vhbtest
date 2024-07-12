4c95a5a62833046bd57fe5481ec8142c0f253078
fneumann <fneumann@databay.de>
2024-07-12 15:46:45 +0200

    Test config and startup script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vhbtest
-Test Environment for the vhbShibAuth Plugin
+Testumgebung für das vhbShibAuth-Plugin
+
+* Dieses Repository in einen `.env` Ordner der ILIAS-Installation kopieren
+* Die `shib_test.conf` in die Apache-Konfiguration eintragen und die Pfade zur ILIAS-Installation anpassen
+* Server-Variablen zur Simulation der vhb-Schnittstelle in `shib_test.php` anpassen
+
+Normaler Aufruf (id ist ein optionaler Deep Link zum vhb-Kurs:
+* http://localhost/projects/ilias9/__vhb__?id=LV_463_1227_1_67_1
+
+Aufruf mit Aktivierung des Plugin-Testmodus:
+
+* http://localhost/projects/ilias9/__vhb__?test=fred
---
+++ shib_test.php
@@ -0,0 +1,23 @@
+<?php
+
+// must be set for shib_login to work
+$_SERVER['HTTP_SHIB_APPLICATION_ID'] = 'vhb';
+$_SERVER['Shib-Application-ID'] = 'vhb';
+$_SERVER['REDIRECT_Shib_Application_ID'] = 'vhb';
+
+// personal data (mapping is configured in Shibboleth settings of ILIAS
+$_SERVER['eduPersonPrincipalName'] = 'vhbtest@uni-erlangen.de';
+//$_SERVER['eduPersonPrincipalName'] = '123457X25@vhb.org';
+$_SERVER['givenName'] = 'Vhb';
+$_SERVER['sn'] = 'Test';
+$_SERVER['mail'] = 'noreply@databay.de';
+
+// data for VhbShibAuth plugin
+$_SERVER['eduPersonEntitlement'] = implode(';', [
+    'urn:mace:vhb.org:entitlement:vhb-access',
+    'urn:mace:vhb.org:entitlement:lms:student:uni-erlangen.de:LV_463_1227_1_67_1',
+    //'urn:mace:vhb.org:entitlement:lms:student:uni-erlangen.de:LV_463_1227_1_67_1'
+]);
+
+chdir("../..");
+require_once('shib_login.php');
