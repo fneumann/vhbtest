50780c654558cfc57bd59ccedb126f40a674756c
fneumann <fneumann@databay.de>
2026-03-26 12:14:58 +0100

    Adapt for ILIAS 10

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # vhbtest
 Testumgebung für das vhbShibAuth-Plugin
 
+## Test mit ILIAS 10 in Unterpfad von localhost
+
 * Dieses Repository in einen `extern` Ordner der ILIAS-Installation kopieren
-* Die `shib_test.conf` in die Apache-Konfiguration eintragen und die Pfade zur ILIAS-Installation anpassen
+* Die `shib_localhost.conf` in die Apache-Konfiguration eintragen und die Pfade zur ILIAS-Installation anpassen
 * Server-Variablen zur Simulation der vhb-Schnittstelle in `shib_test.php` anpassen
 
 Normaler Aufruf (id ist ein optionaler Deep Link zum vhb-Kurs):
-* http://localhost/projects/ilias9/__vhb__?id=LV_463_1227_1_67_1
+* http://localhost/projects/ilias10/public/__vhb__?id=LV_463_1227_1_67_1
 
 Aufruf mit Aktivierung des Plugin-Testmodus:
 
-* http://localhost/projects/ilias9/__vhb__?test=fred
+* http://localhost/projects/ilias10/public/__vhb__?test=fred
+
+# Test mit ILIAS 10 unter ilias10.rx.databay.de
+
--- shib_test.php
+++ shib_test.php
@@ -3,6 +3,7 @@
 // must be set for shib_login to work
 $_SERVER['HTTP_SHIB_APPLICATION_ID'] = 'vhb';
 $_SERVER['Shib-Application-ID'] = 'vhb';
+$_SERVER['Shib-Session-ID'] = '_abc1234567890def';
 $_SERVER['REDIRECT_Shib_Application_ID'] = 'vhb';
 
 // personal data (mapping is configured in Shibboleth settings of ILIAS
@@ -15,9 +16,13 @@ $_SERVER['mail'] = 'noreply@databay.de';
 // data for VhbShibAuth plugin
 $_SERVER['eduPersonEntitlement'] = implode(';', [
     'urn:mace:vhb.org:entitlement:vhb-access',
+
     'urn:mace:vhb.org:entitlement:lms:student:uni-erlangen.de:LV_SINGLE',
-//    'urn:mace:vhb.org:entitlement:lms:student:uni-erlangen.de:LV_463_1227_1_67_1'
+//    'urn:mace:vhb.org:entitlement:lms:evaluation:uni-erlangen.de:LV_SINGLE',
+
+    'urn:mace:vhb.org:entitlement:lms:student:uni-erlangen.de:LV_463_1227_1_67_1',
+//    'urn:mace:vhb.org:entitlement:lms:appr:uni-erlangen.de:LV_463_1227_1_67_1',
 ]);
 
-chdir("../..");
+chdir("../../public");
 require_once('shib_login.php');
